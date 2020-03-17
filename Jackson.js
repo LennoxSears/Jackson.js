dfcca24b69e9f365b553ddc84b492d438bbffc01
lennox <chuangkegongchang@chuangkegongchangdeMacBook-Pro-5.local>
2020-03-17 17:25:24 +0800

    moonWalk update

--- Jackson.js
+++ Jackson.js
@@ -34,7 +34,7 @@
  	Jackson.dance.shake = function(element, range, rotate, speed) {
  		Jackson.dance.stop(element);
  		if(element.style != null) {
- 			var inter = setInterval(function() {
+ 			let inter = setInterval(function() {
 	 			let stepX = Jackson.utl.randomInt(range);
 	 			let stepY = Jackson.utl.randomInt(range);
 	 			let angle = Jackson.utl.randomInt(rotate);
@@ -49,7 +49,55 @@
 
  	Jackson.dance.moonWalk = function(element) {
  		Jackson.dance.stop(element);
- 		//Todo
+ 		let posX = 0, posY = 0, rotA = 0;
+ 		let speed = 5;
+ 		//calculation, control the walk.
+ 		let walkStep = {
+ 			"x" : 0,
+ 			"y" : 0
+ 		}
+ 		let walkSpeed = 6;
+ 		//calculation, control the rotation direction
+		let rDirection = 0; 
+		let rSpeed = 0;
+		//generate initial step
+		walkStep.x = Jackson.utl.randomInt(walkSpeed);
+		walkStep.y = Jackson.utl.randomInt(walkSpeed);
+		rDirection = Jackson.utl.randomInt(rSpeed);
+
+ 		if(element.style != null) {
+ 			let inter = setInterval(function() {
+
+ 				//Detect touching viewport edge
+ 				let eleRect = element.getBoundingClientRect();
+ 				if(eleRect.top < 0 ) {
+ 					walkStep.y = Math.abs(walkStep.y)
+ 				}
+ 				if(viewport_height - eleRect.bottom < 0 ) {
+ 					walkStep.y = Math.abs(walkStep.y) * -1
+ 				}
+ 				if(eleRect.left < 0 ) {
+ 					walkStep.x = Math.abs(walkStep.x)
+ 				}
+ 				if(viewport_width - eleRect.right < 0 ) {
+ 					walkStep.x = Math.abs(walkStep.x) * -1
+ 				}
+ 				
+	 			posX += (walkStep.x);
+	 			posY += (walkStep.y);
+	 			rotA += (rDirection);
+
+
+	 			//Todo
+	 			//element.style.transform-origin = "center center";
+	 			element.style.transform = 'rotate(' + rotA + 'deg)' + 'translate(' + posX +'px, ' + posY + 'px)';
+	 			//element.style.-webkit-transform = 'rotate(' + angle + 'deg)' + 'translate(' + stepX +'px, ' + stepY + 'px)';
+
+	 			console.log(JSON.stringify(eleRect) + ' ' + JSON.stringify(walkStep) + ' ' + rDirection)
+	 		}, 500 / speed);
+
+	 		Jackson.dance.intervals.set(element, inter);
+ 		}
  	}
 
  	exports.Jackson = Jackson;
